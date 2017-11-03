e9d4734809d8c8830a64d76fe6dcf2969f801424
annapismenna <pismennaanna@gmail.com>
2017-11-03 18:53:58 +0700

    Social Links

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
+import './SocialLinks.css';
+import FacebookSvg from './asserts/FacebookSvg';
+import InstagramSvg from './asserts/InstagramSvg';
+
 const SocialLinks = () => {
-    return <div>SocialLinks</div>;
+    return (
+        <div className='social-links'>
+            <FacebookSvg/>
+            <InstagramSvg/>
+        </div>
+    );
 }
 export default SocialLinks;
