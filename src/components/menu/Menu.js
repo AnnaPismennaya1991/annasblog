cd8e8180bb129ecc8b42c43ba1d10cf636f23d83
annapismenna <pismennaanna@gmail.com>
2017-11-10 20:46:24 +0700

    Main Menu final version

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Menu, Icon } from 'antd';
 import 'antd/lib/menu/style/index.css';
-import './Menu.css'
+
+import SearchSvg from '../asserts/SearchSvg';
+import './Menu.css';
 
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
@@ -20,22 +22,39 @@ export default class MenuComponent extends React.Component {
 
     render() {
         return (
-            <Menu className='main-menu'
-                onClick={this.handleClick}
-                selectedKeys={[this.state.current]}
-                mode="horizontal">
-                <Menu.Item key="main">Главная</Menu.Item>
-                <SubMenu title={<span>Категории</span>}>
-                    <MenuItemGroup title="Страны">
-                        <Menu.Item key="country:1">Вьетнам</Menu.Item>
-                        <Menu.Item key="country:2">Таиланд</Menu.Item>
-                        <Menu.Item key="country:3">Малайзия</Menu.Item>
-                        <Menu.Item key="country:4">Индонезия</Menu.Item>
-                    </MenuItemGroup>
-                </SubMenu>
-                <Menu.Item key="galery">Галерея</Menu.Item>
-                <Menu.Item key="about-me">Об авторе</Menu.Item>
-            </Menu>
+            <div className='menu-component'>
+                <Menu className='main-menu'
+                    onClick={this.handleClick}
+                    selectedKeys={[this.state.current]}
+                    mode="horizontal">
+                    <Menu.Item key="main">Главная</Menu.Item>
+                    <SubMenu title={<span>Страны</span>}>
+                        <SubMenu key="sub0" title='Вьетнам'>
+                            <Menu.Item key="1-1">Ханой</Menu.Item>
+                            <Menu.Item key="2-2">Дананг</Menu.Item>
+                            <Menu.Item key="3-3">Хойан</Menu.Item>
+                        </SubMenu>
+                        <SubMenu key="sub1" title='Таиланд'>
+                            <Menu.Item key="2-1">Краби</Menu.Item>
+                            <Menu.Item key="2-2">Аонанг</Menu.Item>
+                            <Menu.Item key="2-3">Бангкок</Menu.Item>
+                            <Menu.Item key="2-4">Пхукет</Menu.Item>
+                        </SubMenu>
+                        <SubMenu key="sub2" title='Малайзия'>
+                            <Menu.Item key="3-1">Лангкави</Menu.Item>
+                        </SubMenu>
+                        <SubMenu key="sub3" title='Индонезия'>
+                            <Menu.Item key="4-1">Бали</Menu.Item>
+                        </SubMenu>
+                    </SubMenu>
+                    <Menu.Item key="galery">Галерея</Menu.Item>
+                    <Menu.Item key="about-me">Об авторе</Menu.Item>
+                </Menu>
+                <div className='search-svg'>
+                
+                    <SearchSvg/>
+                </div>
+            </div>
         );
     }
 }
